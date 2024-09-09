755edb7a07d8e6a0796e545d682d4b4744f25ff4
Luisa Sant'Ana <luisasantana@gmail.com>
2024-09-09 16:02:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,13 +2,29 @@ function pesquisar() {
     // Obtém a seção onde os resultados serão exibidos
     let section = document.getElementById("resultados-pesquisa");
 
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if (campoPesquisa == "") {
+        section.innerHTML = "<p>Nada foi encontrado</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Inicializa uma string vazia para armazenar os resultados
     let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
 
     // Itera sobre os dados da pesquisa
     for (let dado of dados) {
-        // Constrói o HTML para cada item do resultado
-        resultados += `
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
             <div class="item-resultado">
                 <h2>
                     <a href=${dado.link} target="_blank">${dado.titulo}</a>
@@ -17,6 +33,15 @@ function pesquisar() {
                 <a href=${dado.link} target="_blank">Saiba Mais</a>
             </div>
         `;
+        }
+        console.log(dado.titulo.includes(campoPesquisa))
+        // Constrói o HTML para cada item do resultado
+       
+    }
+
+
+    if(!resultados) {
+        resultados = "<p>Esse destino não está na nossa base de dados</p>"
     }
 
     // Atribui o HTML gerado à seção de resultados
--- dados.js
+++ dados.js
@@ -2,20 +2,20 @@ let dados = [
     {
         titulo : "Jalapão",
         descricao : "O Jalapão é um paraíso ecológico localizado no Tocantins. Com suas dunas, fervedouros e cachoeiras cristalinas, o Jalapão oferece uma experiência única para os amantes da natureza. A região é ideal para quem busca aventura e contato com a natureza.",
-        link : "http://www.jalapão.to.gov.br"
+        link : "http://www.jalapão.to.gov.br",
+        tags : "jalapao aventura tocantins"
     },
     {
         titulo : "Bonito",
         descricao : "Bonito é um dos destinos mais procurados para o ecoturismo no Brasil. Com rios cristalinos, grutas, cachoeiras e uma rica biodiversidade, a cidade oferece diversas atividades de aventura e contemplação da natureza. O mergulho em águas cristalinas é uma das principais atrações.",
-        link : "http://www.bonito.ms.gov.br"
+        link : "http://www.bonito.ms.gov.br",
+        tags : "mato grosso mato grosso do sul aventura"
     },
     {
         titulo : "Chapada Diamantina",
         descricao : "A Chapada Diamantina é um dos destinos mais exuberantes da Bahia. Com cachoeiras, cânions, trilhas e paisagens deslumbrantes, a região oferece diversas opções de aventura e contato com a natureza. As cidades de Lençóis e Mucugê são os principais centros turísticos da região.",
-        link : "http://www.chapadadiamantina.ba.gov.br"
-    },
-    {
-        titulo : "Lençóis Maranhenses"
+        link : "http://www.chapadadiamantina.ba.gov.br",
+        tags : "bahia aventura"
     }
 ];
 
